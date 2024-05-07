8fdbe3a16d221474556d850302c435f92a1a3df4
rusty <blaze121304@gmail.com>
2024-05-07 17:49:33 +0900

    [2024.05.07] singleton

---
+++ src/main/java/com/rusty/singleton/ConnectionPool.java
@@ -0,0 +1,17 @@
+package com.rusty.singleton;
+
+public class ConnectionPool {
+
+    private static ConnectionPool instance = new ConnectionPool();
+    private ConnectionPool(){
+
+    }
+
+    public static ConnectionPool getInstance(){
+
+        if(instance == null) {
+            instance = new ConnectionPool();
+        }
+        return instance;
+    }
+}
